9101e7f51c4688b463cd6da84d7b306dfcf8f877
wat2117-tech <wat2117@columbia.edu>
2026-01-22 13:51:46 -0500

    change

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,7 +3,8 @@ function Projects() {
     <div className="p-8 text-white bg-slate-900 min-h-screen">
       <h1 className="text-4xl">Projects</h1>
       <p className ="text-slate-50 text-4xl px-50 mt-50">No Projects LOL this is the project.</p>
-      
+      <script type="text/javascript" src="https://www.juicer.io/embed/cuwmc/embed-code.js" async defer></script>
+
     </div>
   )
 }
